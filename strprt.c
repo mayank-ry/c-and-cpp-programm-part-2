59ce008357c50717973db5d3399cdd5168a6291d
Mayank-R <ray723260@gmail.com>
2025-06-16 13:41:28 +0530

    Add files via upload

---
+++ strprt.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char s1[100], s2[100];
+    int i;
+    printf("Enter String To Print : ");
+    gets(s1);
+    printf("string is : ");
+    puts(s1);
+    return 0;
+}
